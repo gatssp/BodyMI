a0d691f09bad169efe698a30bbffeef027c51745
gatssp <110286579+gatssp@users.noreply.github.com>
2022-12-10 23:23:35 +0300

    Update the name of variables .java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double w, double h) {
+    public double calculate(double weight , double height ) {
         double index = w / (h * h);
         return index;
     }
