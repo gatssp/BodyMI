b0357d8160875b4a70a1c5f5774cd03c156152a4
Polina Shebysheva <gatsbysober@gmail.com>
2023-01-08 14:10:06 +0300

    code reformate

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double weight , double height ) {
+    public double calculate(double weight, double height) {
         double index = weight / (height * height);
         return index;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bodyMassIndex = service.calculate(57, 1.67);
+        double bodyMassIndex = service.calculate(55, 1.67);
 
         System.out.println(bodyMassIndex);
 
